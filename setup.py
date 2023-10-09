f0419aefb0f7fe0e9817014e45ecb22794beb2a5
ang.a <ang.a@bm.technion.ac.il>
2023-10-09 14:13:14 +0300

    github actions

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="super-ivim-dc",
     version="0.1",
     packages=find_packages(),
+    install_requires=requirements, 
     entry_points={
         'console_scripts': [
             'super-ivim-dc = super_ivim_dc.main:main',
